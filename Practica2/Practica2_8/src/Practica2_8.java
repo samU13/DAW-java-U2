634c68d1854029ba3820a1c9f97a17b3b376e71b
samu <samu13lop@gmail.com>
2022-02-19 17:13:50 +0100

    nuevo ejercicio

---
+++ Practica2/Practica2_8/src/Practica2_8.java
@@ -0,0 +1,12 @@
+public class Practica2_8 {
+
+    public static void main(String[] args) {
+        int suma = 0, i; //
+
+        for (i = 1; i <= 100; i++)
+            suma = suma + i * i;
+
+        System.out.println("La suma de los cuadrados de los números "
+                + "del 1 al 100 es " + suma + ".");
+    }
+}
