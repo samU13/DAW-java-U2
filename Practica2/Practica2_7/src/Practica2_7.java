49b4203ed00f48e2ea0ad87edd79fe82434f4566
samu <samu13lop@gmail.com>
2022-02-19 17:12:11 +0100

    nuevo ejercicio

---
+++ Practica2/Practica2_7/src/Practica2_7.java
@@ -0,0 +1,8 @@
+public class Practica2_7 {
+    public static void main(String[] args) {
+        int i;
+
+        for(i = 2; i<=20; i += 2)
+            System.out.println(i + " al cuadrado = " + (i*i));
+    }
+}
